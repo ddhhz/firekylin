e0cdfc6cee2dd142986674a20b15fc0ec42c64e7
Wei <git@weispot.com>
2018-05-18 15:57:39 +0000

    Updated latest config

--- production.js
+++ production.js
@@ -4,7 +4,10 @@ const Application = require('thinkjs');
 const instance = new Application({
   ROOT_PATH: __dirname,
   APP_PATH: path.join(__dirname, 'src'),
-  proxy: true, // use proxy
+//  proxy: true, // use proxy
+  RESOURCE_PATH: __dirname,
+  UPLOAD_PATH: path.join(__dirname, 'static/upload'),
+  UPLOAD_BASE_URL: 'https://⚡.whe.me',
   env: 'production'
 });
 
